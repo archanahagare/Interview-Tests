496b2de119b7aae084558159ecc1794d7756930a
viragb-cuelogic <virag.brahme@cuelogic.co.in>
2016-03-07 12:35:40 +0530

    Json Parsing

--- MyApplication/app/src/main/java/com/example/cuelogic/interview_test/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/cuelogic/interview_test/MainActivity.java
@@ -2,16 +2,87 @@ package com.example.cuelogic.interview_test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.cuelogic.interview_test.ApplicationController.AppController;
+import com.example.cuelogic.interview_test.Model.Animal;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+    private static String TAG = MainActivity.class.getSimpleName();
+    // json response url
+    private String urlJson = "http://192.168.10.104/imageData.php";
+    private String jsonResponse = "";
+    private TextView txtResponse;
+    private List<Animal> animalsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+        makeJsonObjectRequest();
+    }
+
+    /**
+     * Method to make json object request where json response starts wtih {
+     */
+    private void makeJsonObjectRequest() {
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+                urlJson, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    JSONArray animals = response.optJSONArray("animals");
+
+                    for (int i = 0; i < animals.length(); i++) {
+                        JSONObject animalsArr = (JSONObject) animals.get(i);
+                        String name = animalsArr.getString("name");
+                        String imgURL = animalsArr.getString("imgURL");
+
+                        jsonResponse += "Name: " + name + "\n\n";
+                        jsonResponse += "imgURL: " + imgURL + "\n\n";
+                    }
+
+                    txtResponse.setText(jsonResponse);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
     }
 
 
---
+++ MyApplication/app/src/main/java/com/example/cuelogic/interview_test/Model/Animal.java
@@ -0,0 +1,22 @@
+package com.example.cuelogic.interview_test.Model;
+
+public class Animal {
+    String name;
+    String imgURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+}
